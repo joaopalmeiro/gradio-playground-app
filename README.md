06b42aab5f216837e0bb4f9e2bcbb4d6b2251c7b
João Palmeiro <joaommpalmeiro@gmail.com>
2021-02-19 14:21:34 +0000

    Update output and README

--- README.md
+++ README.md
@@ -6,8 +6,14 @@
 2. `pipenv shell`.
 3. `python app.py`.
 
+## Notes
+
+- [`clipboard.js`](https://clipboardjs.com/):
+  - [CDN Providers](https://github.com/zenorocha/clipboard.js/wiki/CDN-Providers).
+- [Gradio](https://www.gradio.app/).
+  - [CSS](https://github.com/gradio-app/gradio/blob/master/gradio/static/css/style.css).
+
 ## References
 
 - "[Alternating caps](https://en.wikipedia.org/wiki/Alternating_caps)" Wikipedia page.
-- [Gradio](https://www.gradio.app/).
 - Abubakar Abid's "[How to Deploy a Machine Learning UI on Heroku in 5 Steps](https://towardsdatascience.com/how-to-deploy-a-machine-learning-ui-on-heroku-in-5-steps-b8cd3c9208e6)" blog post.
--- app.py
+++ app.py
@@ -6,14 +6,33 @@ import gradio as gr
 def alternating_caps(input):
     # Source: https://stackoverflow.com/a/42939226.
     funcs = cycle([str.lower, str.upper])
+    iNpUt = "".join(next(funcs)(c) for c in input.strip())
 
-    return "".join(next(funcs)(c) for c in input.strip())
+    html = """
+    <div class="examples">
+    <button style="background-color: white; border: solid 1px lightgray;" class="btn" data-clipboard-action="copy" data-clipboard-target="div.output_text">
+    Copy
+    </button>
+
+    <script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.6/dist/clipboard.min.js"></script>
+    
+    <script>
+    var clipboard = new ClipboardJS('.btn');
+
+    clipboard.on('success', function (e) {
+        console.log(e);
+    });
+    </script> 
+    </div>
+    """
+
+    return iNpUt, html
 
 
 iface = gr.Interface(
     fn=alternating_caps,
     inputs=gr.inputs.Textbox(lines=1, placeholder="...", label="Input"),
-    outputs="text",
+    outputs=[gr.outputs.Textbox(label="Output"), gr.outputs.HTML(label="Clipboard")],
     verbose=True,
     title="Alternating caps",
     show_tips=False,
